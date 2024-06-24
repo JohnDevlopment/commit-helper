3ddb6aefd564c88131262ecf5739383260589f8e
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-06-24 18:36:38 -0500

    [REFACTOR] Main: Comments; change elif's to if's

--- commit_helper/__main__.py
+++ commit_helper/__main__.py
@@ -1,38 +1,39 @@
-# dependencies imports
-from pathlib import Path
-# utils imports
+# pylint: disable=missing-module-docstring
+from __future__ import annotations
 from .utils.utils import parser_cli
 from .utils.text_utils import debug
 from .utils.file_handler import handle_file_based_commit
 from .utils.file_handler import get_file_path
 from .utils.flag_commit_handler import convention_flag_handler
-# convention imports
 from .conventions.convention_help_handler import convention_help_handler
 
-
 def main():
+    # pylint: disable=missing-function-docstring
     parser = parser_cli()
     args = parser.parse_args()
-    debug_mode = args.debug
 
+    debug_mode: bool = args.debug
     debug('args variable', args, debug_mode)
 
     file_path = get_file_path()
-
     debug('file_path', file_path, debug_mode)
 
-    if args.show_convention_tags is True:
+    # This argument is a bool; it indicates whether to show
+    # the rules of the selected convention
+    if args.show_convention_tags:
         convention_help_handler(file_path, args, debug_mode)
         return
 
-    elif args.convention is not '':
+    # The CONVENTION argument was provided, so we write a
+    # message using that convention
+    if args.convention:
         convention_flag_handler(args, debug_mode)
         return
 
-    elif file_path.is_file():
+    # No convention was provided, so we try to read a file
+    if file_path.is_file():
         handle_file_based_commit(file_path, debug_mode, args)
         return
 
-    else:
-        debug('parser full return', parser.parse_args(), debug_mode)
-        parser.print_help()
+    debug('parser full return', parser.parse_args(), debug_mode)
+    parser.print_help()
