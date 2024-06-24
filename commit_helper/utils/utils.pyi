8c4502724e7ca01152b357d7ba0173a4b2726b41
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-06-23 19:55:29 -0500

    [ADD] utils: utils.pyi type stub

---
+++ commit_helper/utils/utils.pyi
@@ -0,0 +1,25 @@
+from argparse import ArgumentParser, Namespace
+from .protocols import ConfigFile
+from typing import Any, Literal, TypeAlias
+
+Convention: TypeAlias = Literal['angular', 'atom', 'karma', 'symphony', 'tagged']
+
+supported_conventions: list[Convention]
+
+def create_file(convention_name: str, dont_create: bool=...) -> None:
+    ...
+
+def dump_convention(config_file: ConfigFile) -> Any:
+    ...
+
+def gen_co_author(co_author: str) -> str:
+    ...
+
+def handle_convention_commit(convention: str, args: Namespace) -> str:
+    ...
+
+def parser_cli() -> ArgumentParser:
+    ...
+
+def validate_commiter_file(stream_file: ConfigFile) -> None:
+    ...
