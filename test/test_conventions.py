6cae618f36d28915d4612da056c4aee4472f5a87
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-06-24 19:55:54 -0500

    [REFACTOR] Tests/Conventions: Imports, comments, etc.
    Changed imports, fixed test asserts,
    monkey-patched where appropriate.

--- test/test_conventions.py
+++ test/test_conventions.py
@@ -1,35 +1,53 @@
+# pylint: disable=missing-module-docstring
+import builtins
+
+from pytest import MonkeyPatch
 import yaml
-import commit_helper.conventions.atom as atom
-import commit_helper.conventions.tagged as tagged
-import commit_helper.conventions.karma_angular as angular
-import commit_helper.conventions.symphony_cmf as symphony
-import commit_helper.conventions.custom_convention as custom
-import commit_helper.conventions.no_convention as no_convention
 
+from commit_helper.conventions import (
+    atom,
+    custom_convention as custom,
+    karma_angular as angular,
+    symphony_cmf as symphony,
+    no_convention,
+    tagged
+)
 
-def test_karma_angular_convention_with_context():
-    message = angular.karma_angular_convention('TAG', 'message', 'context')
-    if not (message == 'tag(context): message\n'):
-        raise AssertionError()
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-class-docstring
 
+class TestNoConvention:
+    def test_no_convention_without_args(self, monkeypatch: MonkeyPatch):
+        inputs = ["message"]
 
-def test_karma_angular_convention_without_context():
-    message = angular.karma_angular_convention('tag', 'message', '')
-    if not (message == 'tag: message\n'):
-        raise AssertionError()
+        def mock_input(_s):
+            return inputs.pop(0)
+        # monkeypatch.setattr("commit_helper.conventions.no_convention.input", mock_input)
+        monkeypatch.setattr(builtins, "input", mock_input)
 
+        message = no_convention.just_message()
+        assert message == "Message\n"
+
+    def test_no_convention_with_args(self):
+        message = no_convention.just_message("Message")
+        assert message == "Message\n"
+
+class TestKarmaAngularConvention:
+    def test_with_context(self):
+        message = angular.karma_angular_convention('TAG', 'message', 'context')
+        assert message == 'tag(context): message\n'
+
+    def test_karma_angular_convention_without_context(self):
+        message = angular.karma_angular_convention("tag", "message", "")
+        assert message == "tag: message\n"
 
 def test_tagged_convention():
-    message = tagged.tagged_convention('tag', 'message')
-    if not (message == 'TAG: message\n'):
-        raise AssertionError()
-
+    message = tagged.tagged_convention("tag", "message")
+    assert message == "TAG: message\n"
 
 def test_symphony_convention():
-    message = symphony.symphony_convention('tag', 'message')
-    if not (message == '[Tag] message\n'):
-        raise AssertionError()
-
+    message = symphony.symphony_convention("tag", "message")
+    assert message == "[Tag] message\n"
 
 def test_custom_convention():
     fyle = yaml.safe_load("""
@@ -41,31 +59,8 @@ def test_custom_convention():
     msg = "a nice function"
     tag = "add"
     message = custom.custom_convention(tag, msg, fyle, True)
-    if not message == "add..:..a nice function\n":
-        raise AssertionError()
-
-
-def test_no_convention_without_args():
-    inputs = [
-        "message",
-    ]
-
-    def mock_input(s):
-        return inputs.pop(0)
-
-    no_convention.input = mock_input
-    message = no_convention.just_message()
-    if not message == 'Message\n':
-        raise AssertionError()
-
-
-def test_no_convention_with_args():
-    message = no_convention.just_message('Message')
-    if not message == 'Message\n':
-        raise AssertionError()
-
+    assert message == "add..:..a nice function\n"
 
 def test_atom_convention():
-    message = atom.atom_convention('CaNary', 'STUFF')
-    if not message == ":canary: Stuff\n":
-        raise AssertionError()
+    message = atom.atom_convention("CaNary", "STUFF")
+    assert message == ":canary: Stuff\n"
