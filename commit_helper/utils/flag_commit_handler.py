e3af40537d48c0d700f2d87a52ea15f0ecf8f57d
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-06-24 20:27:18 -0500

    [REFACTOR] Utils: convention_flag_handler, imports

--- commit_helper/utils/flag_commit_handler.py
+++ commit_helper/utils/flag_commit_handler.py
@@ -1,30 +1,26 @@
-# dependencies imports
 from os import system
-# utils imports
 from .text_utils import debug
-from .utils import create_file
-from .utils import gen_co_author
-from .utils import handle_conventioned_commit
-# conventions imports
-from commit_helper.conventions.no_convention import just_message
-
+from .utils import create_file, gen_co_author, handle_conventioned_commit
+from ..conventions.no_convention import just_message
 
 def convention_flag_handler(args, debug_mode):
     convention = str(args.convention)
-    debug('convention flag', convention, debug_mode)
-    commit_message = ''
+    debug("convention flag", convention, debug_mode)
+    commit_message = ""
 
-    if convention == 'message':
-        if args.message is not '':
+    if convention == "message":
+        if args.message:
             commit_message = just_message(msg=args.message)
         else:
             commit_message = just_message()
-        convention = 'none'
+        convention = "none"
 
     else:
         commit_message = handle_conventioned_commit(convention, args)
 
     create_file(convention, args.no_file)
+
     commit_message += gen_co_author(args.co_author)
-    debug('commit message', commit_message, debug_mode)
-    system('git commit -m "%s"' % commit_message)
+    debug("commit message", commit_message, debug_mode)
+
+    system(f"git commit -m '{commit_message}'")
