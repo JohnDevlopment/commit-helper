7b4287d97783a9fa244babafb1aab9f50b24bddb
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-10-26 01:06:15 -0500

    [REFACTOR] Pylint: .conventions.karma_angular

--- commit_helper/conventions/karma_angular.py
+++ commit_helper/conventions/karma_angular.py
@@ -1,9 +1,16 @@
+"""
+`karma` convention.
+"""
+
 def karma_angular_convention(tag, msg, context):
+    # pylint: disable=missing-function-docstring
     tag = tag.lower()
+
     if context == '':
         composed_message = "%s: %s\n" % (tag, msg)
     else:
         composed_message = "%s(%s): %s\n" % (tag, context, msg)
+
     return composed_message
 
 
